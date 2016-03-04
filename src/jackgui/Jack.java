d8a4c381c51f42bc73c97cfbc256c5940b5f9200
chenys1995 <argorok7@gmail.com>
2016-03-04 23:12:45 +0800

    implement class district

--- src/jackgui/Jack.java
+++ src/jackgui/Jack.java
@@ -1,5 +1,41 @@
 package jackgui;
 import jackgui.JackGUI;
+class District{
+	//class member
+	public char id;
+	private int deadroad;//only one road is dead
+    private boolean dead = false;
+	//class constructer
+	public District(char c){
+		id=c;
+	}
+	// set the direction of card; degree = 0~270
+    public void set_direction(int deg){
+    	this.deadroad = deg;
+    } 
+    //observer 
+    public int get_deg(){
+    	return deadroad;
+    } 
+    public void die() {
+        if(dead == false){
+		System.console().printf("%c dies", id);
+        this.dead = true;
+		}
+    }; // turn over the card
+    public boolean isDie() {
+        return dead;
+    };
+}
+class Holmes_team{
+	
+	
+}
+class Desktop{
+	
+	
+	
+}
 public class Jack {
 	
 	public static void main(String[] args) {
